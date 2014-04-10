eae49c20c53bb82225fee3e84e0a2d1f6b8d1e9b
Devin Smith <dsmith@walleyesoftware.com>
2014-04-10 10:03:44 -0500

    quickstart

---
+++ src/main/java/org/devin/hazelcast/App.java
@@ -0,0 +1,13 @@
+package org.devin.hazelcast;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
